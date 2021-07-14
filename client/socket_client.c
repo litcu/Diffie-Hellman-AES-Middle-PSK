29dc29dbb94b83a6c7514066c37eeea49102e6dc
qiacu <qiacu@qq.com>
2021-07-14 21:33:41 +0800

    Add AES, but not work correctly

--- client/socket_client.c
+++ client/socket_client.c
@@ -6,12 +6,15 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include "DH.h"
 #include "aes_client.h"
+#include "DH.h"
 
 #define MAX 1024
 
 void exchange_dh_key(int sockfd, mpz_t s);
+void msg_to_pt(char *plain_text);
+void send_encryp_msg(int sockfd, unsigned char *cipher_text);
+void recv_encryp_msg(int sockfd, unsigned char *cipher_text);
 
 int main(int argc, char **argv)
 {
@@ -46,14 +49,62 @@ int main(int argc, char **argv)
     mpz_t dh_s;
     mpz_init(dh_s);
     exchange_dh_key(sockfd, dh_s);
-    gmp_printf("s1 = %Zd\n", dh_s);
-    mpz_clear(dh_s);
+
+    // 声明AES加密解密及通信所需要的变量
+    unsigned char plain_text[MAX], key[32];
+    unsigned char cipher_text[MAX + EVP_MAX_BLOCK_LENGTH], tag[100];
+    unsigned char pt[MAX + EVP_MAX_BLOCK_LENGTH];
+    unsigned char iv[16];
+    unsigned char aad[16] = "abcdefghijklmnop";
+    int rst;
+    mpz_get_str(key, 16, dh_s); // 将dh_s写入key
+    gmp_printf("DH协议商讨出的密钥为：%Zd\n", dh_s);
+    mpz_clear(dh_s); // 清除dh_s
+    if (!PKCS5_PBKDF2_HMAC_SHA1(key, strlen(key), NULL, 0, 1000, 32, key))
+    {
+        printf("AES密钥生成错误！\n");
+        exit(-1);
+    }
+    printf("处理后的AES密钥为：%s\n", key);
+
+    while (!RAND_bytes(iv, sizeof(iv)))
+        ;
+
+    while (1)
+    {
+        bzero(plain_text, MAX);
+        printf("要发送的明文: ");
+        scanf("%s", plain_text);
+        // AES加密，密文存储爱cipher_text
+        bzero(cipher_text, MAX + EVP_MAX_BLOCK_LENGTH);
+        encrypt(plain_text, strlen(plain_text), aad,
+                sizeof(aad), key, iv, cipher_text, tag);
+        write(sockfd, cipher_text, sizeof(cipher_text)); // 发送密文
+        printf("发送的密文：%s\n\n", cipher_text);
+
+        // 接收服务器发送的密文
+        bzero(cipher_text, MAX + EVP_MAX_BLOCK_LENGTH);
+        read(sockfd, cipher_text, sizeof(cipher_text));
+        printf("接收到的密文：%s\n", cipher_text);
+
+        // 解密
+        bzero(pt, MAX + EVP_MAX_BLOCK_LENGTH);
+        int rst = decrypt(cipher_text, sizeof(cipher_text), aad,
+                          sizeof(aad), tag, key, iv, pt);
+        if (rst > 0)
+        {
+            pt[rst] = '\0';
+            printf("解密后的明文：%s\n", pt);
+        }
+        else
+            printf("解密失败！\n");
+        printf("\n\n");
+    }
 
     return 0;
 }
 
-// exchange msg and get the AES secert key
-// by Diffie Hellman protocol
+// 通过Diffie Hellman协议商讨出一个密钥s
 void exchange_dh_key(int sockfd, mpz_t s)
 {
     DH_key client_dh_key;
--- server/socket_server.c
+++ server/socket_server.c
@@ -6,11 +6,15 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include "aes_server.h"
 #include "DH.h"
 
 #define MAX 1024
 
 void exchange_dh_key(int sockfd, mpz_t s);
+void msg_to_pt(char *plain_text);
+void send_encryp_msg(int sockfd, unsigned char *cipher_text);
+void recv_encryp_msg(int sockfd, unsigned char *cipher_text);
 
 int main(int argc, char **argv)
 {
@@ -58,11 +62,62 @@ int main(int argc, char **argv)
     mpz_t dh_s;
     mpz_init(dh_s);
     exchange_dh_key(connfd, dh_s);
-    gmp_printf("%Zd\n", dh_s);
-    mpz_clear(dh_s);
+
+    // 声明AES加密解密所需要的变量
+    unsigned char plain_text[MAX], key[32];
+    unsigned char cipher_text[MAX + EVP_MAX_BLOCK_LENGTH], tag[100];
+    unsigned char pt[MAX + EVP_MAX_BLOCK_LENGTH];
+    unsigned char iv[16];
+    unsigned char aad[16] = "abcdefghijklmnop";
+    int rst;
+    mpz_get_str(key, 16, dh_s); // 将dh_s写入key
+    gmp_printf("DH协议商讨出的密钥为：%Zd\n", dh_s);
+    mpz_clear(dh_s); // 清除dh_s
+    if (!PKCS5_PBKDF2_HMAC_SHA1(key, strlen(key), NULL, 0, 1000, 32, key))
+    {
+        printf("AES密钥生成错误！\n");
+        exit(-1);
+    }
+    printf("处理后的AES密钥为：%s\n", key);
+
+    while (!RAND_bytes(iv, sizeof(iv)))
+        ;
+
+    while (1)
+    {
+        // 接收客户端发送的密文
+        bzero(cipher_text, MAX + EVP_MAX_BLOCK_LENGTH);
+        read(connfd, cipher_text, sizeof(cipher_text));
+        printf("接收到的密文：%s\n", cipher_text);
+
+        // 解密
+        bzero(pt, MAX + EVP_MAX_BLOCK_LENGTH);
+        int rst = decrypt(cipher_text, sizeof(cipher_text), aad,
+                          sizeof(aad), tag, key, iv, pt);
+        if (rst > 0)
+        {
+            pt[rst] = '\0';
+            printf("解密后的明文：%s\n", pt);
+        }
+        else
+            printf("解密失败！\n");
+
+        // 向客户端发送数据
+        bzero(plain_text, MAX);
+        printf("要发送的明文: ");
+        scanf("%s", plain_text);
+
+        // 加密明文
+        bzero(cipher_text, MAX + EVP_MAX_BLOCK_LENGTH);
+        encrypt(plain_text, strlen(plain_text), aad,
+                sizeof(aad), key, iv, cipher_text, tag);
+        write(sockfd, cipher_text, sizeof(cipher_text)); // 发送密文
+        printf("发送的密文：%s\n\n\n", cipher_text);
+    }
 
     // After chatting close the socket
     close(sockfd);
+    return 0;
 }
 
 void exchange_dh_key(int sockfd, mpz_t s)
